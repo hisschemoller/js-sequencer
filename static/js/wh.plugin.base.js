ebf45339a64cf101fa603d0f369297901cfa3784
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2016-04-20 18:15:18 +0200

    File for abstract plugin objects added.

---
+++ static/js/wh.plugin.base.js
@@ -0,0 +1,18 @@
+/**
+ * Plugin base objects. These are abstract and only meant to be extended.
+ * PluginBase defines the base plugin properties that all plugin types use.
+ * PluginGenerator is the base sound generator plugin.
+ * PluginProcessor is the base sound processing plugin.
+ * PluginAnalyser is the base sound analysis plugin.
+ *
+ * @namespace WH
+ */
+window.WH = window.WH || {};
+
+(function (WH) {
+
+    function pluginAbstract() {
+        
+    }
+
+})(WH);
