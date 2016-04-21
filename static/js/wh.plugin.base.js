7a7faccda7b8a83d3b0c17ec0b0822fabd0ccfe3
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2016-04-21 20:18:36 +0200

    Trial setup using functional inheritance.

--- static/js/wh.plugin.base.js
+++ static/js/wh.plugin.base.js
@@ -1,18 +1,87 @@
 /**
- * Plugin base objects. These are abstract and only meant to be extended.
- * PluginBase defines the base plugin properties that all plugin types use.
- * PluginGenerator is the base sound generator plugin.
- * PluginProcessor is the base sound processing plugin.
- * PluginAnalyser is the base sound analysis plugin.
+ * Plugin base objects.
  *
  * @namespace WH
  */
+
 window.WH = window.WH || {};
 
 (function (WH) {
 
-    function pluginAbstract() {
-        
+    function createPlugins(options) {
+
+        var that;
+
+        that = {};
+        return that;
     }
 
+    WH.plugins = createPlugins();
+
 })(WH);
+
+(function (WH, WX) {
+
+    function createPlugin(options, my) {
+
+        var that,
+            to = function(target) {
+
+            },
+            cut = function() {
+
+            },
+            setParam = function (param, arg) {
+
+            },
+            getParam = function (param) {
+                return
+            },
+            setPreset = function (preset) {
+
+            },
+            getPreset = function () {
+                return null;
+            };
+
+        my = my || {};
+
+        that = {};
+        that.to = to;
+        that.cut = cut;
+        that.setParam = setParam;
+        that.getParam = getParam;
+        that.setPreset = setPreset;
+        that.getPreset = getPreset;
+        return that;
+    }
+
+    function createGeneratorPlugin(options, my) {
+
+        var that,
+            output = WX.Gain(),
+            outlet = WX.Gain();
+
+        my = my || {};
+
+        output.to(outlet);
+
+        that = createPlugin(options);
+        return that;
+    }
+
+    WH.createGeneratorPlugin = createGeneratorPlugin;
+
+})(WH, WX);
+
+
+
+(function (WH, WX) {
+
+    function createSynth() {
+
+    }
+
+    WH.createSynth = createSynth;
+
+})(WH, WX);
