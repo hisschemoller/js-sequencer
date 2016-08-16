f99fbfcfa78a49242c9fdc1722111a3ac4376192
Wouter Hisschemöller <wouter.hisschemoller@gmail.com>
2016-08-16 19:31:53 +0200

    Noise plugin plays pink and brown noise.

--- static/js/plugin/wh.plugin.noise.js
+++ static/js/plugin/wh.plugin.noise.js
@@ -12,21 +12,30 @@ window.WH = window.WH || {};
 
         var that,
             voices = [],
-            cachedWhiteNoise,
+            channels = 1,
+            noiseType,
+            noiseCache,
+            bufferSize,
+
+            init = function() {
+                bufferSize = my.core.getSampleRate() * (60 / my.transport.getBPM()) * 2;
+                noiseCache = {
+                    white: null,
+                    pink: null,
+                    brown: null
+                };
+            },
 
             /**
              * Create a buffer of white noise.
              * @return {object} AudioBuffer of white noise.
              */
             createWhiteNoise = function() {
-                if (cachedWhiteNoise) {
-                    return cachedWhiteNoise;
+                if (noiseCache.white) {
+                    return noiseCache.white;
                 }
 
-                var channels = 1,
-                    sampleRate = my.core.getSampleRate(),
-                    bufferSize = sampleRate * (60 / my.transport.getBPM()) * 2,
-                    buffer = my.core.createBuffer(channels, bufferSize, sampleRate),
+                var buffer = my.core.createBuffer(channels, bufferSize, my.core.getSampleRate()),
                     bufferChannels = buffer.getChannelData(0),
                     i;
 
@@ -34,7 +43,65 @@ window.WH = window.WH || {};
                     bufferChannels[i] = Math.random() * 2 - 1;
                 }
 
-                cachedWhiteNoise = buffer;
+                noiseCache.white = buffer;
+
+                return buffer;
+            },
+
+            /**
+             * Create a buffer of pink noise.
+             * @return {object} AudioBuffer of pink noise.
+             */
+            createPinkNoise = function() {
+                if (noiseCache.pink) {
+                    return noiseCache.pink;
+                }
+
+                var buffer = my.core.createBuffer(channels, bufferSize, my.core.getSampleRate()),
+                    bufferChannels = buffer.getChannelData(0),
+                    white, i, b0, b1, b2, b3, b4, b5, b6;
+
+                b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+                for (i = 0; i < bufferSize; i++) {
+                    white = Math.random() * 2 - 1;
+                    b0 = 0.99886 * b0 + white * 0.0555179;
+                    b1 = 0.99332 * b1 + white * 0.0750759;
+                    b2 = 0.96900 * b2 + white * 0.1538520;
+                    b3 = 0.86650 * b3 + white * 0.3104856;
+                    b4 = 0.55000 * b4 + white * 0.5329522;
+                    b5 = -0.7616 * b5 - white * 0.0168980;
+                    bufferChannels[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+                    bufferChannels[i] *= 0.11;
+                    b6 = white * 0.115926;
+                }
+
+                noiseCache.pink = buffer;
+
+                return buffer;
+            },
+
+            /**
+             * Create a buffer of brown noise.
+             * @return {object} AudioBuffer of brown noise.
+             */
+            createBrownNoise = function() {
+                if (noiseCache.brown) {
+                    return noiseCache.brown;
+                }
+
+                var buffer = my.core.createBuffer(channels, bufferSize, my.core.getSampleRate()),
+                    bufferChannels = buffer.getChannelData(0),
+                    i, white,
+                    lastOut = 0.0;
+
+                for (i = 0; i < bufferSize; i++) {
+                    white = Math.random() * 2 - 1;
+                    bufferChannels[i] = (lastOut + (0.02 * white)) / 1.02;
+                    lastOut = bufferChannels[i];
+                    bufferChannels[i] *= 3.5; // (roughly) compensate for gain
+                }
+
+                noiseCache.brown = buffer;
 
                 return buffer;
             },
@@ -47,9 +114,9 @@ window.WH = window.WH || {};
              */
             createVoice = function(pitch, velocity, time) {
                 var voice = {
-                    bufferSource: my.core.createBufferSource(),
-                    gain: my.core.createGain()
-                };
+                        bufferSource: my.core.createBufferSource(),
+                        gain: my.core.createGain()
+                    };
 
                 if (voices[pitch]) {
                     noteOff(pitch);
@@ -57,7 +124,19 @@ window.WH = window.WH || {};
 
                 voices[pitch] = voice;
 
-                voice.bufferSource.buffer = createWhiteNoise();
+                switch (noiseType) {
+                    case 'white':
+                        voice.bufferSource.buffer = createWhiteNoise();
+                        break;
+                    case 'pink':
+                        voice.bufferSource.buffer = createPinkNoise();
+                        break;
+                    case 'brown':
+                        voice.bufferSource.buffer = createBrownNoise();
+                        break;
+                }
+
+                
                 voice.bufferSource.to(voice.gain).to(my.output);
                 voice.bufferSource.start(time);
             },
@@ -80,19 +159,21 @@ window.WH = window.WH || {};
             noisetype: 'white'
         };
         my.$noisetype = function(value, time, rampType) {
-            
+            noiseType = value;
         };
         
         that = WH.createGeneratorPlugin(specs, my);
-        
+
         my.defineParams({
             noisetype: {
                 type: 'itemized',
                 name: 'Noise Type',
                 default: 'white',
                 model:  my.conf.getModel('noisetypes')
-            },
+            }
         });
+
+        init();
             
         that.noteOn = noteOn;
         that.noteOff = noteOff;
