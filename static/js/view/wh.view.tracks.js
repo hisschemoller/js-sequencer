6bc506d67b92270af665ebd772b5e326ff623d74
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2016-08-16 16:15:21 +0200

    Tracks view shows selected steps in track colors.

--- static/js/view/wh.view.tracks.js
+++ static/js/view/wh.view.tracks.js
@@ -12,38 +12,53 @@ window.WH = window.WH || {};
             conf = specs.conf,
             trackCount = conf.getTrackCount(),
             stepCount = conf.getStepCount(),
+            stepEls = [],
+            columns = [],
+            activeColomnIndex,
             
             /**
              * Initialize the view, create the track lanes.
              */
             init = function() {
-                var i, j, trackEl, stepEl,
+                var i, j, trackEl, stepEl, colorClass,
                     trackTemplate = $('#template-tracks-track'),
                     stepTemplate = $('#template-tracks-step');
                 for (i = 0; i < trackCount; i++) {
                     trackEl = trackTemplate.children().first().clone();
                     my.rootEl.append(trackEl);
+                    stepEls.push([]);
+                    colorClass = conf.getColor(i);
                     for (j = 0; j < stepCount; j++) {
                         stepEl = stepTemplate.children().first().clone();
+                        stepEl.addClass(colorClass);
                         trackEl.append(stepEl);
+                        stepEls[i].push(stepEl);
                     }
+                }
+                
+                // cache column selectors for playback animation.
+                for (i = 0; i < trackCount; i++) {
+                    columns.push(my.rootEl.children('.tracks__track').find(':nth-child(' + (i + 1) + ')'));
                 }
             },
             
             /**
              * Update all tracks of a pattern.
+             * A step plays if it's velocity is greater than 0.
+             * @param {object} patternData Pattern data includes all it's tracks and steps.
              */
             setPattern = function(patternData) {
                 var i, j, trackData, stepData;
                 for (i = 0; i < trackCount; i++) {
                     trackData = patternData.getTrackSteps(i);
                     for (j = 0; j < stepCount; j++) {
-                        
-                        if (trackData[j].getVelocity()) {
-                            
-                        }
+                        stepEls[i][j].toggleClass('selected', trackData[j].getVelocity() > 0);
                     }
                 }
+            },
+            
+            setActiveStep = function() {
+                
             };
             
         var my = my || {};
